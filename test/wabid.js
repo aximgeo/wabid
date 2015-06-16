ccbe6844950f7a35ef933c2978c3effdc15eafc3
Ian Firkin <ian.firkin@gisinc.com>
2015-06-16 11:32:02 -0700

    Adds basic file checks for wab files

--- test/wabid.js
+++ test/wabid.js
@@ -1,8 +1,54 @@
 var expect = require('chai').expect,
-    wabid = require('../wabid');
+    wabid = require('../wabid'),
+    mock = require('mock-fs');
 
 describe('wabid', function() {
-    it('Has a method to determine if a directory contains a WAB widget', function () {
-        expect(wabid.isWidget).to.exist;
+
+    describe("Checking for WAB files", function () {
+
+        afterEach(function () {
+            mock.restore();
+        });
+
+        it('Has a method to determine if a directory contains a WAB widget', function () {
+            expect(wabid.isWidget).to.exist;
+        });
+
+        it('Returns true if all the files exist in the directory', function () {
+            var files = [
+                    'manifest.json',
+                    'Widget.js'
+                ];
+            mock({
+                'my/mock/directory': {
+                   'Widget.js': '',
+                   'manifest.json': ''
+               }
+           });
+            expect(wabid.isWidget('my/mock/directory/', files)).to.be.true;
+        });
+
+        it('Uses a default file list if not given one', function () {
+            mock({
+                'my/mock/directory': {
+                    'Widget.js': '',
+                    'manifest.json': ''
+                }
+            });
+            expect(wabid.isWidget('my/mock/directory/')).to.be.true;
+        });
+
+        it('Returns false if any of the files do not exist in the directory', function () {
+            var files = [
+                'manifest.json',
+                'Widget.js'
+            ];
+            mock({
+                'my/mock/directory': {
+                    'manifest.json': ''
+                }
+            });
+            expect(wabid.isWidget('my/mock/directory/', files)).to.be.false;
+        });
     });
 });
--- wabid.js
+++ wabid.js
@@ -1,10 +1,36 @@
+var fs = require('fs'),
+walk = require('walk');
+
 module.exports = {
 
     /**
      * Returns true if the directory contains a WAB widget
      * @param dir
      */
-    isWidget: function (dir) {
+    isWidget: function(dir, files) {
+        var wabFiles = files || [
+            'manifest.json',
+            'Widget.js'
+        ];
+        var self = this;
+        return wabFiles.map(function (file) {
+            return dir + file;
+        }).every(function (file) {
+                return self.exists(file);
+        });
+    },
 
+    exists: function (filePath) {
+        var fileExists = true, fd;
+        try {
+            fd = fs.openSync(filePath, 'r');
+            fs.closeSync(fd);
+        }
+        catch (e) {
+            fileExists = false;
+        }
+        finally {
+            return fileExists;
+        }
     }
 }
